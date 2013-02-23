7d97f4a435608ebd6c605bb563b2af36d9b9d209
Kasper Souren <kasper@guaka.org>
2013-02-23 03:18:14 +0100

    dont fetch external resources, fix #9

--- meteor2cordova.sh
+++ meteor2cordova.sh
@@ -9,7 +9,8 @@ echo Turning $URL into a Phonegap app
 echo Make sure an unminified Meteor app is running on $URL
 echo "(i.e. on localhost or through meteor deploy --debug)"
 rm -rf cordovaapp $URL
-wget -e robots=off -E -H -k -K -p $URL
+
+wget -e robots=off -E -k -K -p $URL
 
 cordova create cordovaapp
 cd cordovaapp
