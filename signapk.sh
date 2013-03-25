35e2327d905ab9a248c416dea29d3a46480455ec
Kasper Souren <kasper@guaka.org>
2013-03-25 14:57:46 +0100

    source ~/.meteor2cordova

--- signapk.sh
+++ signapk.sh
@@ -1,17 +1,23 @@
+
+
+# Read settings
+source ~/.meteor2cordova
+
 cd cordovaapp/platforms/android
 
 echo We need your keystore alias
-YOURALIAS=$1
+YOURALIAS=${1-$M2C_KEY_ALIAS}
+
 
 # To create keystore:
-# keytool -validity 9125 -sigalg MD5withRSA -keyalg RSA -keysize 1024 -genkey -v -alias $YOURALIAS -keystore ~/release-key.keystore
+# keytool -validity 9125 -sigalg MD5withRSA -keyalg RSA -keysize 1024 -genkey -v -alias $YOURALIAS -keystore $M2C_KEY_CERTFILE
 
 echo Build release version of apk
 ant release
 
 UNSIGNED=$(find .|grep apk$|grep release-unsigned)
-echo signing $UNSIGNED
-jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/release-key.keystore $UNSIGNED $YOURALIAS
+echo signing $2
+echo $M2C_KEY_PASS | jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore $M2C_KEY_CERTFILE $UNSIGNED $YOURALIAS
 
 echo 'zipalign your apk'
 zipalign 4 $UNSIGNED ready.apk
