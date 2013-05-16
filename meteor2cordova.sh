f27ed271f945f0150bbc6b1fb3f05409513e31e1
Kasper Souren <kasper@guaka.org>
2013-05-16 19:15:26 +0200

    removed .xml downloads

--- meteor2cordova.sh
+++ meteor2cordova.sh
@@ -35,42 +35,34 @@ echo -e "\nApplying the main hack: some JS that overrides _toSockjsUrl"
 sed -i.bak 's#</head>#<script type="text/javascript">Meteor._Stream._toSockjsUrl = function(e) { return "http://$URL/sockjs" }</script></head>#g' www/index.html
 
 
-echo -e "\nChecking for public/cordvova/config.xml"
-wget -nv --directory-prefix=$DLDIR -e robots=off $URL/cordova/config.xml
-echo -e "checking for public/cordvova/AndroidManifest.xml"
-wget -nv --directory-prefix=$DLDIR -e robots=off $URL/cordova/AndroidManifest.xml
-
-
-
-if grep -Fq "<?xml version" $DLDIR/config.xml; then
-    echo 'Found config.xml in public/cordova'
-    cp $DLDIR/config.xml www/
+pwd
+if [ -e ../config.xml ]; then
+    echo 'Using local config.xml'
+    cp ../config.xml www/
 else
-    if [ -e ../config.xml ]; then
-        echo 'Using local config.xml'
-        cp ../config.xml www/
-    else
-        echo "Didn't find your config.xml, will hack the default one for you, you probably want to copy this to public/cordova/ of your Meteor project"
-        NAME=$(echo $URL|sed s/\[\\.\:\]/\./g)
-        echo "Widget id: $NAME"
-        sed -i.bak s/hellocordova/$NAME/ www/config.xml
-        sed -i.bak s/HelloCordova/$URL/g www/config.xml
-    fi
+    echo "Didn't find your config.xml, will hack the default one for you, you probably want to copy this to public/cordova/ of your Meteor project"
+    NAME=$(echo $URL|sed s/\[\\.\:\]/\./g)
+    echo "Widget id: $NAME"
+    sed -i.bak s/hellocordova/$NAME/ www/config.xml
+    sed -i.bak s/HelloCordova/$URL/g www/config.xml
 fi
+
+
 
 echo -e "\nNow building android platform"
 cordova platform add android
 
 
-if grep -Fq "<?xml version" $DLDIR/AndroidManifest.xml; then
-    echo 'Found AndroidManifest.xml in public/cordova'
-    cp $DLDIR/AndroidManifest.xml platforms/android/
-else
-    if [ -e ../AndroidManifest.xml ]; then
-        echo 'Using local AndroidManifest.xml'
-        cp ../AndroidManifest.xml platforms/android/
-    fi
+if [ -e ../build.xml ]; then
+    echo 'Using local build.xml'
+    cp ../build.xml platforms/android/
 fi
+
+if [ -e ../AndroidManifest.xml ]; then
+    echo 'Using local AndroidManifest.xml'
+    cp ../AndroidManifest.xml platforms/android/
+fi
+
 
 
 
